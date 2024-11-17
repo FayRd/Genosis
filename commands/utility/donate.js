c254f8c60d288d20060157265abcc2e36b678a05
Ression <true.ression@gmail.com>
2024-11-17 18:40:05 +0800

    Added /donate command

--- commands/utility/donate.js
+++ commands/utility/donate.js
@@ -27,18 +27,8 @@ module.exports = {
         const primaryEmbed = new EmbedBuilder()
             .setColor("#e6a7b2")
             .setTitle('Donate me!')
-            //.setURL('https://discord.js.org/')
-            .setAuthor({ name: 'Ression', iconURL: 'https://imgur.com/l3IN61a.png', url: 'https://github.com/FayRd/Genosis-Discord-Bot' })
             .setDescription('Kindly support me through donations!')
-            //.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            // .addFields(
-            // 	{ name: 'Regular field title', value: 'Some value here' },
-            // 	{ name: '\u200B', value: '\u200B' },
-            // 	{ name: 'Inline field title', value: 'Some value here', inline: true },
-            // 	{ name: 'Inline field title', value: 'Some value here', inline: true },
-            // )
-            //.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-            //.setImage('https://i.imgur.com/AfFp7pu.png')
+            .setThumbnail(interaction.client.user.displayAvatarURL({ size: 64 }))
             .setTimestamp()
             .setFooter({ text: 'Powered by RΞSSIФИ\'s love', iconURL: 'https://imgur.com/E2apyia.png' });
         await interaction.reply({embeds: [primaryEmbed], components: [row]});
