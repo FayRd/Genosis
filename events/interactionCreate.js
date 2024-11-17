5f94f43113f57db36d91b818c7c08500253cba59
Ression <true.ression@gmail.com>
2024-11-17 13:16:35 +0800

    Update error message

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -40,9 +40,9 @@ module.exports = {
 		} catch (error) {
 			console.error(error);
 			if (interaction.replied || interaction.deferred) {
-				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+				await interaction.followUp({ content: 'Um.. sorry I have problems executing that command for you! >^<', ephemeral: true });
 			} else {
-				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+				await interaction.reply({ content: 'Um.. sorry I have problems executing that command for you! >^<', ephemeral: true });
 			}
 		}
 	},
