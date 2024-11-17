2f8c9f5f16e27cbde3f73d9ad8ed63d1323165ae
Ression <true.ression@gmail.com>
2024-11-17 13:17:02 +0800

    Added embed message to /ping

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 
 module.exports = {
 	cooldown: 5,
@@ -15,9 +15,23 @@ module.exports = {
 			.setLabel('Stop!')
 			.setStyle(ButtonStyle.Danger);
 		const row = new ActionRowBuilder().addComponents(ping, stop);
-		await interaction.reply({
-			content: 'Pong!',
-			components: [row],
-		});
+		const primaryEmbed = new EmbedBuilder()
+		.setColor("#e6a7b2")
+		.setTitle('I am replying!')
+		//.setURL('https://discord.js.org/')
+		.setAuthor({ name: 'Ression', iconURL: 'https://imgur.com/l3IN61a.png', url: 'https://github.com/FayRd/Genosis-Discord-Bot' })
+		.setDescription('Pong!')
+		//.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+		// .addFields(
+		// 	{ name: 'Regular field title', value: 'Some value here' },
+		// 	{ name: '\u200B', value: '\u200B' },
+		// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+		// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+		// )
+		//.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+		//.setImage('https://i.imgur.com/AfFp7pu.png')
+		.setTimestamp()
+		.setFooter({ text: 'Powered by RΞSSIФИ\'s love', iconURL: 'https://imgur.com/E2apyia.png' });
+		await interaction.reply({embeds: [primaryEmbed], components: [row]});
 	},
 };
