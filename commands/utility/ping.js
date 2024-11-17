1fca9d12e023bc0af86acdfae7c199f598ea6c84
Ression <true.ression@gmail.com>
2024-11-17 18:40:20 +0800

    Improvised /ping command

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -4,34 +4,48 @@ module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Get the assistant\'s ping!'),
 	async execute(interaction) {
 		const ping = new ButtonBuilder()
 			.setCustomId('Ping!')
-			.setLabel('Ping!')
+			.setLabel('Ping again!')
 			.setStyle(ButtonStyle.Primary);
-		const stop = new ButtonBuilder()
-			.setCustomId('Stop!')
-			.setLabel('Stop!')
-			.setStyle(ButtonStyle.Danger);
-		const row = new ActionRowBuilder().addComponents(ping, stop);
+		const row = new ActionRowBuilder().addComponents(ping);
 		const primaryEmbed = new EmbedBuilder()
-		.setColor("#e6a7b2")
-		.setTitle('I am replying!')
-		//.setURL('https://discord.js.org/')
-		.setAuthor({ name: 'Ression', iconURL: 'https://imgur.com/l3IN61a.png', url: 'https://github.com/FayRd/Genosis-Discord-Bot' })
-		.setDescription('Pong!')
-		//.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-		// .addFields(
-		// 	{ name: 'Regular field title', value: 'Some value here' },
-		// 	{ name: '\u200B', value: '\u200B' },
-		// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
-		// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
-		// )
-		//.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-		//.setImage('https://i.imgur.com/AfFp7pu.png')
-		.setTimestamp()
-		.setFooter({ text: 'Powered by RΞSSIФИ\'s love', iconURL: 'https://imgur.com/E2apyia.png' });
-		await interaction.reply({embeds: [primaryEmbed], components: [row]});
+			.setColor("#deeb34")
+			.setTitle('Wait.. hold on!')
+			.setDescription('Gimme a second...')
+			.setTimestamp()
+			.setFooter({ text: 'Powered by RΞSSIФИ\'s love', iconURL: 'https://imgur.com/E2apyia.png' });
+		let circles = {
+			good: '<:High:1307619015945879572>',
+			okay: '<:Mid:1307619003681865819> ',
+			bad: '<:Low:1307618980944416768>',
+		};
+		// Defer reply
+		await interaction.deferReply();
+		// Edit its contents
+		const pinging = await interaction.editReply({ embeds: [primaryEmbed] });
+
+		// Ping
+		const ws = interaction.client.ws.ping;
+		const msgEdit = Date.now() - pinging.createdTimestamp;
+
+		// Emoji
+		const wsEmoji = ws <= 100 ? circles.good : ws <= 200 ? circles.okay : circles.bad;
+		const msgEmoji = msgEdit <= 200 ? circles.good : circles.bad;
+
+		// Update Embed
+		const pingEmbed = EmbedBuilder.from(primaryEmbed)
+			.setColor("#e6a7b2")
+			.setTitle('Here it is!')
+			.setDescription(' ')
+			.setThumbnail(interaction.client.user.displayAvatarURL({ size: 64 }))
+			.addFields(
+				{ name: 'Websocket Latency', value: `${wsEmoji} \`${ws}ms\`` },
+				{ name: 'API Latency', value: `${msgEmoji} \`${msgEdit}ms\`` }
+			);
+		// Result
+		await pinging.edit({ embeds: [pingEmbed], components: [row] });
 	},
 };
